3604388024288c5176c08aeb943a9d5544099b97
tim <tim@tim-Aspire-V3-571G>
2016-05-06 16:55:28 +0800

    bug uppercase

--- JavaWebFoundation/src/conFlow/UpperOrLowerCase.java
+++ JavaWebFoundation/src/conFlow/UpperOrLowerCase.java
@@ -10,18 +10,19 @@ public class UpperOrLowerCase {
 		//TODO 將字串 kITTY sNoopY MiCKY 轉換為 Kitty Snoopy Micky
 		//(第一個字母大寫，其它的字母皆為小寫)
 		Scanner sn = new Scanner(System.in);
-		System.out.println("請輸入英文字元");
-		if (sn.hasNext("[a-zA-Z]")) {
-			if (sn.hasNext("[A-Z]")) {
-				System.out.print("你輸入的是大寫，我來幫你改成小寫: ");
-				System.out.println((sn.next()).toLowerCase());
-			} else{
-				System.out.print("你輸入的是小寫，我來幫你改成大寫: ");
-				System.out.println((sn.next()).toUpperCase());
+		System.out.println("請輸入英文字元或英文語句");
+		if (sn.hasNext("[a-zA-Z]+")) {
+			String [] tt = sn.nextLine().split(" ");
+			for (int a = 0 ; a<= tt.length-1 ; a++) {
+				System.out.println(tt[a].substring(0,1).matches("[A-Z]"));
+				if ((tt[a].substring(0,1)).matches("[A-Z]")) {
+					System.out.print(tt[a].toLowerCase());
+				} else {
+					System.out.print(tt[a].toUpperCase());
+				}
 			}
 		} else {
 			System.out.println("你所輸入的不是英文字母");
 		}
 	}
-
 }
