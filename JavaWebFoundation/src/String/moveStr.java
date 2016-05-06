49f04f5e4a548f60ec3f02779779558548363e0c
tim <tim@tim-Aspire-V3-571G>
2016-05-06 17:41:42 +0800

    test str

---
+++ JavaWebFoundation/src/String/moveStr.java
@@ -0,0 +1,16 @@
+package String;
+
+public class moveStr {
+
+	public static void main(String[] args) {
+		// TODO 字元移動
+		//
+		String tt = "kitty";
+		System.out.println(tt.substring(0, 1));
+		System.out.println(tt.substring(1, tt.length()-1));
+		System.out.println(tt.indexOf("i"));
+		System.out.print(tt.length());
+
+	}
+
+}
