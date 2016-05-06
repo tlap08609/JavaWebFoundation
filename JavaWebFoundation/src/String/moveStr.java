da03e77678afc0bb9356e263da33551845e4b1e2
tim <tim@tim-Aspire-V3-571G>
2016-05-06 18:28:48 +0800

    18:28

--- JavaWebFoundation/src/String/moveStr.java
+++ JavaWebFoundation/src/String/moveStr.java
@@ -1,16 +1,41 @@
 package String;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class moveStr {
 
 	public static void main(String[] args) {
 		// TODO 字元移動
-		//
-		String tt = "kitty";
-		System.out.println(tt.substring(0, 1));
-		System.out.println(tt.substring(1, tt.length()-1));
-		System.out.println(tt.indexOf("i"));
-		System.out.print(tt.length());
+		// ab**cd**e*12 -> *****abcde12
+		System.out.println("請輸入字串:");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char[] ch = str.toCharArray();
+		System.out.println("處理前:"+str);
+		int sum = beginMove(ch);
+		System.out.println("此'*'字元數量為:"+sum);
+		
 
+	}
+	public static int beginMove(char[] ch){
+		int i,j = ch.length-1;
+		for (i = j;j >= 0;j--) {
+			if (ch[i] != '*') {
+				i--;
+			} else if (ch[j] != '*') {
+				ch[i] = ch[j];
+				ch[j] = '*';
+				i--;
+			}
+		}
+			System.out.print("處理後的字串為:");
+			for (int k = 0 ; k < ch.length;k++) {
+				System.out.print(ch[k]);
+			}
+			System.out.println();
+			return i+1;
+		
 	}
 
 }
