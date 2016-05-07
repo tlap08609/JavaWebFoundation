b076f77b910de5c654ee1962584cdadf3a122678
tim <tlap086091@livemail.tw>
2016-05-07 16:12:35 +0800

    new test

--- JavaWebFoundation/src/file/BmpFile.java
+++ JavaWebFoundation/src/file/BmpFile.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class BmpFile {
 
 	public static void main(String[] args) {
+		//http://crazycat1130.pixnet.net/blog/post/1345538-%E9%BB%9E%E9%99%A3%E5%9C%96%EF%BC%88bitmap%EF%BC%89%E6%AA%94%E6%A1%88%E6%A0%BC%E5%BC%8F
+		//利用表頭資訊的2-5的位元組，查出該檔案大小
 		File file = new File("PG30009.bmp");
 		byte data[] = new byte[54];
 		int fileSize,value1,value2,value3,value4,low,high,num;
---
+++ JavaWebFoundation/src/file/OppsiteDownBMP.java
@@ -0,0 +1,10 @@
+package file;
+
+public class OppsiteDownBMP {
+
+	public static void main(String[] args) {
+		// TODO 複製圖片為result.bmp,並讓圖片上下左右顛倒
+
+	}
+
+}
